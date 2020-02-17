d1b5609cfea0ca0bc45e8a7f9125774dc1227302
Lokesh Satpute <lokeshstpt@gmail.com>
2020-02-17 17:48:31 +0800

    adding Thymeleaf-author

--- src/main/java/lokesh/springframework/spring5webapp/domain/Author.java
+++ src/main/java/lokesh/springframework/spring5webapp/domain/Author.java
@@ -50,6 +50,14 @@ public class Author {
         this.books = bookd;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Author{" +
